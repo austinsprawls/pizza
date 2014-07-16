f76d70e6b00afa77ce0f74b2aa066072bf10fffc
Austin Sprawls <austinsprawls@gmail.com>
2014-07-16 09:58:41 -0500

    Initialized topping class and added attr_accessor for name to get tests to pass

--- pizza.rb
+++ pizza.rb
@@ -2,4 +2,13 @@ class Pizza
 end
 
 class Topping
+  attr_accessor :name
+  # Instantiate a new topping.
+  #
+  # name - The String name of topping.
+  #
+  # Returns a new Topping object.
+  def initialize(name)
+    @name = name
+  end
 end
